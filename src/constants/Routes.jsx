10aa63ee74034a7300ea8934ac9b1e14922c5c4e
Tze Ming <tzeming.phang@u.nus.edu>
2020-09-14 13:16:39 +0800

    Add private route

--- src/constants/Routes.jsx
+++ src/constants/Routes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import PrivateRoute from "../modules/components/PrivateRoute";
 import LandingPage from "../modules/pages/LandingPage";
 import GroupBuyPage from "../modules/pages/GroupBuyPage";
 
@@ -7,7 +9,7 @@ const Routes = (props) => (
   <Router>
     <Switch>
       <Route exact path="/" render={() => <LandingPage {...props} />} />
-      <Route path="/groupbuy" render={() => <GroupBuyPage {...props} />} />
+      <PrivateRoute path="/groupbuy" render={() => <GroupBuyPage {...props} />} />
     </Switch>
   </Router>
 );
---
+++ src/modules/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Cookies from "js-cookie";
+
+const PrivateRoute = ({ render, path, ...rest }) => {
+  const newRender = Cookies.get("t1") && Cookies.get("t2") ? render : () => null;
+  return <Route render={newRender} path {...rest} />;
+};
+
+export default PrivateRoute;
