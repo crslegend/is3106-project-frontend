4503f82bd631ab54680e5e1aeedb46ea775d08d7
Tze Ming <tzeming.phang@u.nus.edu>
2020-09-16 16:06:15 +0800

    Set env var for windows

--- src/modules/components/PrivateRoute.jsx
+++ src/modules/components/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Cookies from "js-cookie";
+import PropTypes from "prop-types";
 
 const PrivateRoute = ({ render, path, ...rest }) => {
   const newRender = Cookies.get("t1") && Cookies.get("t2") ? render : () => null;
   return <Route render={newRender} path {...rest} />;
 };
 
-GroupBuyBody.propTypes = {
+PrivateRoute.propTypes = {
   render: PropTypes.func.isRequired,
   path: PropTypes.string.isRequired,
 };
