a6b37b3f99ef8cab7c9b66cec2077b0c2e5335da
crslegend <risheng17@gmail.com>
2020-10-15 16:54:57 +0800

    minor edits for price calculation

--- src/customer/newRecipe/calculatePrice.js
+++ src/customer/newRecipe/calculatePrice.js
@@ -2,7 +2,9 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
   let price = 0;
 
   if (sellingAmount.includes("X") && sellingAmount.endsWith("KG")) {
-    const sellingAmountStr = sellingAmount.split(" ")[2].replace(/\D/g, "");
+    const sellingAmountStr = sellingAmount
+      .split(" ")[2]
+      .replace(/[^.,0-9]/g, "");
     price = parseFloat(
       (parseFloat(sellingPrice) / parseFloat(sellingAmountStr * 1000)) *
         parseFloat(selectedAmount)
@@ -15,7 +17,7 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
       sellingAmount.endsWith("ml"))
   ) {
     const sellingAmountStr =
-      sellingAmount.split(" ")[2].replace(/\D/g, "") *
+      sellingAmount.split(" ")[2].replace(/[^.,0-9]/g, "") *
       sellingAmount.split(" ")[0];
     price = parseFloat(
       (parseFloat(sellingPrice) / parseFloat(sellingAmountStr)) *
@@ -27,7 +29,7 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
   ) {
     const sellingAmountStr =
       1000 *
-      sellingAmount.split(" ")[2].replace(/\D/g, "") *
+      sellingAmount.split(" ")[2].replace(/[^.,0-9]/g, "") *
       sellingAmount.split(" ")[0];
     price = parseFloat(
       (parseFloat(sellingPrice) / parseFloat(sellingAmountStr)) *
@@ -36,9 +38,10 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
   } else if (sellingAmount.endsWith("KG") || sellingAmount.endsWith(" KG")) {
     price = parseFloat(
       (parseFloat(sellingPrice) /
-        (parseFloat(sellingAmount.replace(/\D/g, "")) * 1000)) *
+        (parseFloat(sellingAmount.replace(/[^.,0-9]/g, "")) * 1000)) *
         parseFloat(selectedAmount)
     );
+    console.log(price);
   } else if (
     sellingAmount.endsWith(" G") ||
     sellingAmount.endsWith("G") ||
@@ -49,14 +52,16 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
   ) {
     price = parseFloat(
       (parseFloat(sellingPrice) /
-        parseFloat(sellingAmount.replace(/\D/g, ""))) *
+        parseFloat(sellingAmount.replace(/[^.,0-9]/g, ""))) *
         parseFloat(selectedAmount)
     );
   } else if (
     (sellingAmount.includes("X") || sellingAmount.includes("x")) &&
     sellingAmount.endsWith("per pack)" || sellingAmount.endsWith("Per Pack)"))
   ) {
-    const sellingAmountStr = sellingAmount.split(" ")[3].replace(/\D/g, "");
+    const sellingAmountStr = sellingAmount
+      .split(" ")[3]
+      .replace(/[^.,0-9]/g, "");
     price = parseFloat(
       (parseFloat(sellingPrice) / parseFloat(sellingAmountStr)) *
         parseFloat(selectedAmount)
@@ -65,7 +70,9 @@ const calculatePrice = (sellingPrice, sellingAmount, selectedAmount) => {
     sellingAmount.endsWith("per pack)") ||
     sellingAmount.endsWith("Per Pack)")
   ) {
-    const sellingAmountStr = sellingAmount.split(" ")[1].replace(/\D/g, "");
+    const sellingAmountStr = sellingAmount
+      .split(" ")[1]
+      .replace(/[^.,0-9]/g, "");
     price = parseFloat(
       (parseFloat(sellingPrice) / parseFloat(sellingAmountStr)) *
         parseFloat(selectedAmount)
