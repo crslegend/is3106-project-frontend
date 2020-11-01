f914e9ff9809820e85adfec3e894a8f4c1436121
Phang Tze Ming <e0311042@u.nus.edu>
2020-11-01 23:13:38 +0800

    Add date string util

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,25 @@
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export function capitalizeFirstLetter(string) {
   if (typeof string !== "string") return string;
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function getDateString(string) {
+  if (typeof string !== "string") return string;
+  const date = new Date(string);
+  return `${monthNames[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`;
 }
